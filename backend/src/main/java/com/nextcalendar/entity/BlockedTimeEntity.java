d594989e71eb7704a669ce92e23601b38eb9b3b0
Gabrielli Borba <gabrielliborba42@gmail.com>
2026-08-18 08:20:47 -0300

    feat(appointment): cria entity blockedTime para profissional inserir horários que não estará dispoÃnível para agendamento, sendo folgas ou intervalos específicos

---
+++ backend/src/main/java/com/nextcalendar/entity/BlockedTimeEntity.java
@@ -0,0 +1,40 @@
+package com.nextcalendar.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "blocked_times")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlokedTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professional_id", nullable = false)
+    private ProfessionalEntity professional;
+
+    @Column(nullable = false)
+    private LocalDateTime startDateTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endDateTime;
+
+    private String reason;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+}
